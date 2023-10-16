9a0181833c99f79cfda3e941cca966b3d4e084d8
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-16 10:11:40 -0500

    Se agrego la funcion for con las variable para que el programa nos bote el resultado hasta los 20 primeros multiplos de 3

--- Ejercicio2#15/Ejercicio15.c
+++ Ejercicio2#15/Ejercicio15.c
@@ -2,10 +2,16 @@
 
 int main (){
 
-    int multiplode20;
+    //Kevin Johan Maquis Calle
 
-    printf("Los 20 primeros multiplos de tres son:");
 
+    printf("Los 20 primeros multiplos de tres son: ");
+
+    for (int i = 1; i <= 20; i++)
+    {
+        int multiplode20 = 3*i;
+        printf("%d ", multiplode20);
+    }
     
 
     return 0;
