a33e4e23f8eb6c3636e1fb7886755885c55721a5
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-16 10:02:04 -0500

    Final de progrmama con las funciones y probado

--- Ejercicio1#06/Ejercicio6.c
+++ Ejercicio1#06/Ejercicio6.c
@@ -2,6 +2,8 @@
 
 int main (){
 
+    //Kevin Johan Maquis Calle
+
     int numingresado;
 
     printf("Ingrese el numero de 3 dijitos:\n");
@@ -17,23 +19,24 @@ int main (){
         int dig2 = (numingresado/10)%10;
         int dig3 = numingresado%10;
 
-        printf("Los numeros enteros que se encuentran entre 1 y el primer digito son: ");
+        printf("\nLos numeros enteros que se encuentran entre 1 y el primer digito son: ");
         for (int cont = 1; cont <= dig1; cont++)
         {
             printf("%d ",cont);
         }
 
-        printf("Los numeros enteros que se encuentran entre 1 y el segundo digito son: ");
+        printf("\nLos numeros enteros que se encuentran entre 1 y el segundo digito son: ");
         for (int cont = 1; cont <= dig2; cont++)
         {
             printf("%d ",cont);
         }
 
-
-
+        printf("\nLos numeros enteros que se encuentran entre 1 y el tercer digito son: ");
+        for (int cont = 1; cont <= dig3; cont++)
+        {
+            printf("%d ",cont);
+        }
     }
-    
-
-
+    printf("\nGRACIAS");
     return 0;
 }
