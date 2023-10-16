bc22889915fcd3653503bf76bf3eb66e90b6c51d
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-16 09:30:20 -0500

    Primera funcion para sacar los numerosenteros comprendidosdel 1 al primer  digitos

--- Ejercicio1#06/Ejercicio6.c
+++ Ejercicio1#06/Ejercicio6.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
 
-int main() {
-    int numero;
+int main (){
 
-    // Solicitar al usuario un número de tres digitos
-    printf("Introduce un numero de tres digitos: ");
-    scanf("%d", &numero);
+    int numingresado;
 
-    // Validar si el numero tiene tres digitos
-    if (numero < 100 || numero > 999) {
-        printf("El numero no tiene tres digitos validos.\n");
-        return 1;
+    printf("Ingrese el numero de 3 dijitos:\n");
+    scanf("%d",&numingresado);
+
+    if (numingresado<100||numingresado>999)
+    {
+        printf ("El numero ingresado es incorrecto\n");
     }
+    else
+    {
+        int dig1 = numingresado/100;
+        int dig2 = (numingresado/10)%10;
+        int dig3 = numingresado%10;
 
-    // Extraer los digitos
-    int digito1 = numero / 100;
-    int digito2 = (numero / 10) % 10;
-    int digito3 = numero % 10;
+        printf("Los numeros enteros que se encuentran entre 1 y el primer digito son: ");
+        for (int cont = 1; cont <= dig1; cont++)
+        {
+            printf("%d ",cont);
+        }
+        
 
-    // Mostrar los enteros comprendidos entre 1 y cada digito
-    printf("Enteros comprendidos entre 1 y el primer digito: ");
-    for (int i = 1; i <= digito1; i++) {
-        printf("%d ", i);
     }
-    printf("\n");
+    
 
-    printf("Enteros comprendidos entre 1 y el segundo digito: ");
-    for (int i = 1; i <= digito2; i++) {
-        printf("%d ", i);
-    }
-    printf("\n");
-
-    printf("Enteros comprendidos entre 1 y el tercer digito: ");
-    for (int i = 1; i <= digito3; i++) {
-        printf("%d ", i);
-    }
-    printf("\n");
 
     return 0;
 }
