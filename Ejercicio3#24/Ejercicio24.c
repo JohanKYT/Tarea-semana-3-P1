7b6d74f2291f42f542d54f77957eda5275c782d5
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-16 10:33:29 -0500

    Agregamos la funcion while para crear un bucle y dependiendo del resultado un if para que agregue y sume los numeros pares

--- Ejercicio3#24/Ejercicio24.c
+++ Ejercicio3#24/Ejercicio24.c
@@ -9,5 +9,24 @@ int main(){
     printf("");
     scanf("%d",&numingresado);
 
+    if (numingresado<0)
+    {
+        numingresado=-(numingresado);
+    }
+
+    while (numingresado>0)
+    {
+        dig=numingresado%10;
+        if (dig%2==0)
+        {
+            sumanum=sumanum+dig;
+        }
+        
+
+
+    }
+    
+    
+
     return 0;
 }
