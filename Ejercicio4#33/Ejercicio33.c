a7163493cea1fc9a7579ac68ccb9eb5c175dfc63
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-16 22:48:36 -0500

    Se agrego un for para que ayude a verificar si es un numero primo

--- Ejercicio4#33/Ejercicio33.c
+++ Ejercicio4#33/Ejercicio33.c
@@ -9,6 +9,17 @@ int main(){
 
     for (int i = numsuperior-1; i>=2 ; i--)
     {
+        int PrimoSi=1;
+        for (int j = 2; j*j<=i ; j++)
+        {
+            if (i%j==0)
+            {
+                PrimoSi=0;
+                break;
+            }
+            
+        }
+        
 
     }
 
